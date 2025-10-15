1b4b5cdf4b0a1c729ea37eddc327952d0815c52a
Nikolai Rønman <nikolairoenman@gmail.com>
2025-10-15 14:57:46 +0200

    Name Controller
    Name controller etc

--- Test-Backend/Controllers/NameController.cs
+++ Test-Backend/Controllers/NameController.cs
@@ -1,6 +1,30 @@
-namespace Test_Backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Test_Backend.Services;
+using Test_Backend.Models;
 
-public class NameController
+namespace Test_Backend.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController(NameService nameService) : ControllerBase
+    {
+        [HttpGet("random")]
+        public ActionResult<Name> GetRandom()
+        {
+            return Ok(nameService.GetRandomName());
+        }
+
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<Name>> GetAll()
+        {
+            return Ok(nameService.GetNames());
+        }
+
+        [HttpGet("gender/{gender}")]
+        public ActionResult<IEnumerable<Name>> GetByGender(string gender)
+        {
+            var results = nameService.GetByGender(gender);
+            return Ok(results);
+        }
+    }
 }
--- Test-Backend/Services/NameService.cs
+++ Test-Backend/Services/NameService.cs
@@ -1,6 +1,30 @@
+using System.ComponentModel;
+using System.Text.Json;
+using Test_Backend.Models;
+
 namespace Test_Backend.Services;
 
 public class NameService
 {
+    public List<Name> _names;
+    public Random _random;
+
+    public NameService(IWebHostEnvironment environment)
+    {
+        var filePath = Path.Combine(environment.ContentRootPath, "Data", "person-names.json");
+        var json = File.ReadAllText(filePath);
+        _names = JsonSerializer.Deserialize<List<Name>>(json);
+    }
+
+    public Name GetRandomName()
+    {
+        if (_names.Count == 0)
+            throw new InvalidOperationException("No names found xd)");
+        return _names[_random.Next(_names.Count)];
+    }
+    
+    public IEnumerable<Name> GetNames() => _names;
+    
+    public IEnumerable<Name> GetByGender(string gender) => GetNames().Where(n => n.Gender == gender);
     
 }
