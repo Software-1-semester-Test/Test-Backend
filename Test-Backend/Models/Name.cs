9f79203bb25660faa96a6dde81b34986f5170325
Nikolai Rønman <nikolairoenman@gmail.com>
2025-10-15 14:23:53 +0200

    Model Klasser
    Model kalsser uden contrains

---
+++ Test-Backend/Models/Name.cs
@@ -0,0 +1,9 @@
+namespace Test_Backend.Models;
+
+public class Name
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? Gender { get; set; }
+    
+}
---
+++ Test-Backend/Models/PhoneNumber.cs
@@ -0,0 +1,7 @@
+namespace Test_Backend.Models;
+
+public class PhoneNumber
+{
+    public string Number { get; set; }
+    
+}
