4fe6d0e13e73e48efa10fdee2f172ecdc95bc65a
Nikolai Rønman <nikolairoenman@gmail.com>
2025-10-21 10:43:23 +0200

    Cors update
    Updated cors for frontend

--- Test-Backend/Program.cs
+++ Test-Backend/Program.cs
@@ -4,7 +4,7 @@ using Test_Backend.Services;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Add services to the container.
+// Services
 builder.Services.AddSingleton<NameService>();
 builder.Services.AddSingleton<CprService>();
 builder.Services.AddSingleton<PhoneNumberService>();
@@ -12,25 +12,35 @@ builder.Services.AddTransient<AddressService>();
 builder.Services.AddSingleton<PersonService>();
 builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(connectionString));
 
-
+// Controllers & Swagger
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS for frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Enable CORS
+app.UseCors("AllowFrontend");
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
